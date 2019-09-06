f012014c5d35540de56c326250a29ca20d29c518
asameree <asameree@LR064WAM-1718.ellis.int>
2019-09-06 11:01:50 -0400

    after warmup

---
+++ src/U1L02NumNickCalc.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+//U1L02 Notes
+public class U1L02NumNickCalc {
+
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog(null, "How many cents do you have?");
+		int userMoney = Integer.parseInt(input);
+		
+		//System.out.println(userMoney);
+		
+		int numBNick = userMoney/5;
+		int pennies = userMoney - (numBNick *5);
+		JOptionPane.showMessageDialog(null, userMoney + " cents is " + numBNick + " nickles and " + pennies + " pennies.");
+		
+
+	}
+
+}
