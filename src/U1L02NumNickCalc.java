abfc7ff5b73b87525f96f93bbd1f8429302c54ab
asameree <asameree@LR064WAM-1718.ellis.int>
2019-09-09 21:03:14 -0400

    Finished HW

--- src/U1L02NumNickCalc.java
+++ src/U1L02NumNickCalc.java
@@ -7,10 +7,22 @@ public class U1L02NumNickCalc {
 		String input = JOptionPane.showInputDialog(null, "How many cents do you have?");
 		int userMoney = Integer.parseInt(input);
 		
+		// 25-quarters, 10 -dimes 5 nickle 1 penny
+		//57
+		int numbQuart = userMoney/25;
+		int userMoney1 = userMoney - numbQuart * 25;
+		//numbQuart 2
+		int numbDim = (userMoney%25)/10;
+		userMoney1 = userMoney1 - numbDim * 10;
+
+		//int currUM = userMoney -  
+		//numbdim 2
+		//int numBNick =userMoney%(numbQuart*numbDim);
+		// line above is wrong
 		
-		int numBNick = userMoney/5;
-		int pennies = userMoney - (numBNick *5);
-		JOptionPane.showMessageDialog(null, userMoney + " cents is " + numBNick + " nickles and " + pennies + " pennies.");
+		int numBNick = userMoney1/5;
+		int pennies = userMoney1 - (numBNick *5);
+		JOptionPane.showMessageDialog(null, userMoney + " cents is " + numbQuart + " Quarters " + numbDim + " dimes" + numBNick + " nickles and " + pennies + " pennies.");
 		
 
 	}
